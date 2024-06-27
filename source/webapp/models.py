07f8a65bc4fa0c314043a411d1b561b560d7a09a
”Kirill <kirillgrekhovodov@gmail.com>
2024-06-27 20:03:24 +0600

    добавил модель секции, связал с моделью статьи, изменил создание и детальный просмотр

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 statuses = [("new", "Новая"), ("moderated", "Модерированная"), ("deleted", "Удаленная")]
 
+
+class Section(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Название", unique=True)
+    description = models.CharField(max_length=50, null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.pk}. {self.title}"
+
+    class Meta:
+        db_table = "sections"
+        verbose_name = "Секция"
+        verbose_name_plural = "Секции"
+
+
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Название")
     author = models.CharField(max_length=50, null=False, blank=False, verbose_name="Автор", default="Неизвестный")
@@ -9,6 +23,12 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
     status = models.CharField(max_length=20, choices=statuses, verbose_name="Статус", default=statuses[0][0])
+    section = models.ForeignKey(
+        "webapp.Section",
+        on_delete=models.RESTRICT,
+        verbose_name="Секция",
+        related_name="articles",  # article_set,
+    )
 
     def __str__(self):
         return f"{self.pk}. {self.title}: {self.author}"
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from webapp.models import Article
+from webapp.models import Article, Section
 
 
 def index(request):
@@ -10,12 +10,16 @@ def index(request):
 
 def create_article(request):
     if request.method == "GET":
-        return render(request, "create_article.html")
+        sections = Section.objects.all()
+        return render(request, "create_article.html", {"sections": sections})
     else:
+        # section = Section.objects.get(pk=request.POST["section_id"])
         article = Article.objects.create(
             title=request.POST.get("title"),
             content=request.POST.get("content"),
-            author=request.POST.get("author")
+            author=request.POST.get("author"),
+            section_id=request.POST.get("section_id"),
+            #section = section
         )
         return redirect("article_detail", pk=article.pk)
 
