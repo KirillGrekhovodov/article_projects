0ed8b85abde39c8637b137e7951a143bcdd2c696
”Kirill <kirillgrekhovodov@gmail.com>
2024-07-10 21:11:19 +0600

    в admin.py добавил связь с тегами

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,13 +3,25 @@ from django.contrib import admin
 from webapp.models import Article
 
 
+class TagsInline(admin.TabularInline):
+    model = Article.tags.through
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'created_at']
     list_display_links = ['id', 'title']
     list_filter = ['author']
     search_fields = ['title', 'content']
-    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
+    fields = ['title', 'author', 'content', 'created_at', 'updated_at',]
     readonly_fields = ['created_at', 'updated_at']
+    # filter_horizontal = ["tags"]
+    inlines = [
+        TagsInline,
+    ]
 
 
 admin.site.register(Article, ArticleAdmin)
+
+
+
+
+
