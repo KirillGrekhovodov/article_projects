3ec5e470e5b2f522bea8d5f1cd4897674b0ab0f9
”Kirill <kirillgrekhovodov@gmail.com>
2024-10-02 20:55:57 +0600

    переделали проект с использованием докера, создали файлы docker-compose.yaml и Dockerfile

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -93,14 +93,7 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str("DB_NAME"),
-        'USER': env.str("DB_USER"),
-        'PASSWORD': env.str("DB_PASSWORD"),
-        'HOST': env.str("DB_HOST"),
-        'PORT': env.int("DB_PORT"),
-    }
+    "default": env.db()
 }
 
 REST_FRAMEWORK = {
